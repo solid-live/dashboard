fab1ebe614c5903ce84be6e53dc909bf38147f1d
Melvin Carvalho <melvincarvalho@gmail.com>
2016-04-11 01:55:26 +0200

    Cleanup

--- dist/js/ld.js
+++ dist/js/ld.js
@@ -1,16 +1,35 @@
-
-
-
-/*
- * Author: Abdullah A Almsaeed
- * Date: 4 Jan 2014
- * Description:
- *      This is a demo file used only for the main dashboard (index.html)
- **/
+// GLOBALS
 var view;
-$(function () {
+var kb, g, f;
+var workURI, choresURI, userURI;
+var wallets = [];
+var solid;
+
+var CURR  = $rdf.Namespace("https://w3id.org/cc#");
+var FOAF  = $rdf.Namespace("http://xmlns.com/foaf/0.1/");
+var RDFS  = $rdf.Namespace("http://www.w3.org/2000/01/rdf-schema#");
 
 
+// HELPER FUNCTIONS
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", "i"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
+// INIT FUNCTIONS
+function init() {
+  initView();
+  initRDF();
+  initSolid();
+}
+
+function initView() {
   view = new Vue({
     el: '#app',
     data: {
@@ -73,165 +92,158 @@ $(function () {
     }
   })
 
-  function getParameterByName(name, url) {
-      if (!url) url = window.location.href;
-      name = name.replace(/[\[\]]/g, "\\$&");
-      var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", "i"),
-          results = regex.exec(url);
-      if (!results) return null;
-      if (!results[2]) return '';
-      return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
+/**
+* init RDF knowledge base
+*/
+function initRDF() {
+  var PROXY = "https://data.fm/proxy?uri={uri}";
+
+  //var AUTH_PROXY = "https://rww.io/auth-proxy?uri=";
+  var TIMEOUT = 60000;
+  $rdf.Fetcher.crossSiteProxyTemplate=PROXY;
+
+  g = $rdf.graph();
+  f = $rdf.fetcher(g, TIMEOUT);
+};
+
+function initSolid() {
+  solid = require('solid');
+}
+
+
+// FETCH FUNCTION
+function fetchStats() {
+  var uri = workURI + 'today?source=' + encodeURIComponent(userURI);
+  //var workURI = 'http://melvincarvalho.com:11088/today?source=https://melvincarvalho.com/%23me';
+  console.log('fetching :' + uri);
+  $.get(uri, function( work ) {
+    if (work && work['https://w3id.org/cc#amount']) {
+      view.widgets.summary.work = work['https://w3id.org/cc#amount'];
+    }
+  });
+
+  //var choresURI = 'http://melvincarvalho.com:11089/today?source=https://melvincarvalho.com/%23me';
+  uri = choresURI + 'today?source=' + encodeURIComponent(userURI);
+  console.log('fetching :' + uri);
+  $.get(uri, function( work ) {
+    if (work && work['https://w3id.org/cc#amount']) {
+      view.widgets.summary.tidy = work['https://w3id.org/cc#amount'];
+    }
+  });
+}
+
+function fetchWallets() {
+  initRDF();
+
+  f.nowOrWhenFetched(userURI, undefined, function(ok, body) {
+    console.log('fetched '+ userURI );
+    var w = g.statementsMatching($rdf.sym(userURI), CURR('wallet'));
+    console.log(wallets);
+    for (var i = 0; i < w.length; i++) {
+      var wallet = w[i];
+      var walletURI = wallet.object.uri;
+      console.log('fetching : ' + walletURI);
+      wallets.push(walletURI);
+
+      f.nowOrWhenFetched(walletURI.split('#')[0], undefined, function(ok, body) {
+        console.log('fetched : ' + walletURI);
+        renderWallets();
+      });
+    }
+  });
+
+}
+
+
+// RENDER FUNCTIONS
+function renderWallets() {
+  console.log('rendering wallets');
+  console.log(wallets);
+  for (var i = 0; i < wallets.length; i++) {
+    var wallet = wallets[i];
+    var type = g.any($rdf.sym(wallet), RDFS('label'));
+    console.log('type for ' + wallet + ' is ' + type);
+    if (type.value === 'work') {
+      workURI = g.any($rdf.sym(wallet), CURR('api')).uri;
+      console.log(workURI);
+    }
+    if (type.value === 'chores') {
+      choresURI = g.any($rdf.sym(wallet), CURR('api')).uri;
+      console.log(choresURI);
+    }
+    if (workURI && choresURI) {
+      fetchStats();
+    }
   }
-
-  var kb, g, f;
-
-  $(function () {
-    var wallets = [];
-
-    // $rdf is exported as a global when you load RDFLib, above
-    var solid = require('solid');
-
-    $('#login').on('click', function() {
-      solid.login().then(function(webid){
-        // authentication succeeded; do something with the WebID string
-        console.log(webid);
-        view.user.loggedIn = true;
-        userURI = webid;
-        populateUser(webid);
-      }).catch(function(err) {
-        // authentication failed; display some error message
-        console.log(err)
-      })
-
-    })
+}
 
 
-    /**
-    * init RDF knowledge base
-    */
-    var initRDF = function() {
-      var PROXY = "https://data.fm/proxy?uri={uri}";
+function renderUser(userURI) {
+  solid.identity.getProfile(userURI).then(function (parsedProfile) {
+    console.log('getProfile result: %o', parsedProfile);
+    kb = parsedProfile.parsedGraph;
 
-      //var AUTH_PROXY = "https://rww.io/auth-proxy?uri=";
-      var TIMEOUT = 60000;
-      $rdf.Fetcher.crossSiteProxyTemplate=PROXY;
+    var name = kb.any($rdf.sym(userURI), FOAF('name'));
+    var avatar = kb.any($rdf.sym(userURI), FOAF('depiction')) || kb.any($rdf.sym(userURI), FOAF('img'));
+    console.log(name);
+    console.log(avatar);
+    if (name) {
+      view.user.name = name.value;
+    }
+    if (avatar) {
+      view.user.avatar = avatar.uri;
+    }
+    view.user.loggedIn = true;
 
-      g = $rdf.graph();
-      f = $rdf.fetcher(g, TIMEOUT);
-    };
-
-
-    console.log('solid.js version: ' + solid.meta.version())
-
-
-    var workURI, choresURI;
-    var CURR  = $rdf.Namespace("https://w3id.org/cc#");
-    var FOAF  = $rdf.Namespace("http://xmlns.com/foaf/0.1/");
-    var RDFS  = $rdf.Namespace("http://www.w3.org/2000/01/rdf-schema#");
-
-
-    var userURI = getParameterByName('profile');
-
-    if (!userURI) return;
-    populateUser(userURI);
     fetchWallets();
 
-
-    function fetchWallets() {
-      initRDF();
-
-      f.nowOrWhenFetched(userURI, undefined, function(ok, body) {
-        console.log('fetched '+ userURI );
-        var w = g.statementsMatching($rdf.sym(userURI), CURR('wallet'));
-        console.log(wallets);
-        for (var i = 0; i < w.length; i++) {
-          var wallet = w[i];
-          var walletURI = wallet.object.uri;
-          console.log('fetching : ' + walletURI);
-          wallets.push(walletURI);
-
-          f.nowOrWhenFetched(walletURI.split('#')[0], undefined, function(ok, body) {
-            console.log('fetched : ' + walletURI);
-            renderWallets();
-          });
-        }
-      });
-
-    }
-
-
-    function renderWallets() {
-      console.log('rendering wallets');
-      console.log(wallets);
-      for (var i = 0; i < wallets.length; i++) {
-        var wallet = wallets[i];
-        var type = g.any($rdf.sym(wallet), RDFS('label'));
-        console.log('type for ' + wallet + ' is ' + type);
-        if (type.value === 'work') {
-          workURI = g.any($rdf.sym(wallet), CURR('api')).uri;
-          console.log(workURI);
-        }
-        if (type.value === 'chores') {
-          choresURI = g.any($rdf.sym(wallet), CURR('api')).uri;
-          console.log(choresURI);
-        }
-        if (workURI && choresURI) {
-          fetchStats();
-        }
-      }
-
-    }
-
-    function fetchStats() {
-      var uri = workURI + 'today?source=' + encodeURIComponent(userURI);
-      //var workURI = 'http://melvincarvalho.com:11088/today?source=https://melvincarvalho.com/%23me';
-      console.log('fetching :' + uri);
-      $.get(uri, function( work ) {
-        if (work && work['https://w3id.org/cc#amount']) {
-          view.widgets.summary.work = work['https://w3id.org/cc#amount'];
-        }
-      });
-
-      //var choresURI = 'http://melvincarvalho.com:11089/today?source=https://melvincarvalho.com/%23me';
-      uri = choresURI + 'today?source=' + encodeURIComponent(userURI);
-      console.log('fetching :' + uri);
-      $.get(uri, function( work ) {
-        if (work && work['https://w3id.org/cc#amount']) {
-          view.widgets.summary.tidy = work['https://w3id.org/cc#amount'];
-        }
-      });
-    }
-
-    function populateUser(userURI) {
-      solid.identity.getProfile(userURI).then(function (parsedProfile) {
-        console.log('getProfile result: %o', parsedProfile);
-        kb = parsedProfile.parsedGraph;
-
-        var name = kb.any($rdf.sym(userURI), FOAF('name'));
-        var avatar = kb.any($rdf.sym(userURI), FOAF('depiction')) || kb.any($rdf.sym(userURI), FOAF('img'));
-        console.log(name);
-        console.log(avatar);
-        if (name) {
-          view.user.name = name.value;
-        }
-        if (avatar) {
-          view.user.avatar = avatar.uri;
-        }
-        view.user.loggedIn = true;
-
-        fetchWallets();
-
-        $('#logout').on('click', function() {
-          console.log('logging out...');
-          view.user.loggedIn = false;
-          view.user.avatar = "dist/img/avatar3.png";
-          view.user.name = "Login";
-        })
-
-      });
-
-    }
+    $('#logout').on('click', function() {
+      console.log('logging out...');
+      view.user.loggedIn = false;
+      view.user.avatar = "dist/img/avatar3.png";
+      view.user.name = "Login";
+    })
 
   });
+
+}
+
+// EVENT FUNCTIONS
+function addEvents() {
+  $('#login').on('click', function() {
+    solid.login().then(function(webid){
+      // authentication succeeded; do something with the WebID string
+      console.log(webid);
+      view.user.loggedIn = true;
+      userURI = webid;
+      renderUser(webid);
+    }).catch(function(err) {
+      // authentication failed; display some error message
+      console.log(err)
+    })
+
+  });
+}
+
+
+/*
+ * Author: Abdullah A Almsaeed
+ * Date: 4 Jan 2014
+ * Description:
+ *      This is a demo file used only for the main dashboard (index.html)
+ **/
+$(function () {
+  init();
+  addEvents();
+
+  userURI = getParameterByName('profile');
+
+  if (!userURI) return;
+  renderUser(userURI);
+  fetchWallets();
+
 
 });
